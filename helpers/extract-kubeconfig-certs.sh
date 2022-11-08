bcf520ece862e7f23d34877de18ac49f274ffb77
pablete-im <pablete.im@gmail.com>
2022-11-08 11:27:11 +0100

    TAP1.3.0 required changes for all packages to successfully reconcile

--- helpers/extract-kubeconfig-certs.sh
+++ helpers/extract-kubeconfig-certs.sh
@@ -5,8 +5,8 @@ CLUSTER_NAME_PREFIX=$1
 for PROFILE in view run iterate build 
 do
 	echo "extracting certificates and keys from $i kubeconfig file"
-	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq '.clusters[0].cluster.certificate-authority-data' | base64 -d > tap-${PROFILE}-cacert.pem
-	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq '.users[0].user.client-certificate-data' | base64 -d > tap-${PROFILE}-cert.pem
-	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq '.users[0].user.client-key-data' | base64 -d > tap-${PROFILE}-cert.key
+	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq -r '.clusters[0].cluster."certificate-authority-data"' | base64 -d > tap-${PROFILE}-cacert.pem
+	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq -r '.users[0].user."client-certificate-data"' | base64 -d > tap-${PROFILE}-cert.pem
+	cat kubeconfig${CLUSTER_NAME_PREFIX}${PROFILE}.txt | yq -r '.users[0].user."client-key-data"' | base64 -d > tap-${PROFILE}-cert.key
 done
 
